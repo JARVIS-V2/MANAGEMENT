b9ef68d29fc2fd0d192aadd487c441bd7acb3b30
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-17 00:00:46 +0530

    Update errors.py

--- JARVISHUB/utils/errors.py
+++ JARVISHUB/utils/errors.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 from functools import wraps
-from scenario import pgram, SUPPORT_CHAT
+from JARVISHUB import pgram, SUPPORT_CHAT
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 
