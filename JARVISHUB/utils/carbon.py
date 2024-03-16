ebf89f457e5c9e6f68dcef7b6919859b5ce97570
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-17 00:01:04 +0530

    Update carbon.py

--- JARVISHUB/utils/carbon.py
+++ JARVISHUB/utils/carbon.py
@@ -5,5 +5,5 @@ async def make_carbon(code):
     url = "https://carbonara.vercel.app/api/cook"
     async with aiohttpsession.post(url, json={"code": code}) as resp:
         image = BytesIO(await resp.read())
-    image.name = "Scenario_Carbon.png"
+    image.name = "jarvis_Carbon.png"
     return image
